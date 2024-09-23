b8a12bfbe3da697a7526de8bf7b6ccad7f2a5f74
DKhorkov <dkhorkov.dev@gmail.com>
2024-09-23 18:15:13 +0300

    [HMTM-28] - added database connection and made it works

--- internal/repositories/users_repository.go
+++ internal/repositories/users_repository.go
@@ -2,20 +2,83 @@ package repositories
 
 import (
 	"github.com/DKhorkov/hmtm-sso/entities"
+	"github.com/DKhorkov/hmtm-sso/internal/database"
 	customerrors "github.com/DKhorkov/hmtm-sso/internal/errors"
+	"github.com/DKhorkov/hmtm-sso/internal/interfaces"
 )
 
 type CommonUsersRepository struct {
+	DBConnector interfaces.DBConnector
 }
 
 func (repo *CommonUsersRepository) GetUserByID(id int) (*entities.User, error) {
-	return nil, &customerrors.UserNotFoundError{}
+	user := &entities.User{}
+	columns := database.GetEntityColumns(user)
+	connection := repo.DBConnector.GetConnection()
+	err := connection.QueryRow(
+		`
+			SELECT * 
+			FROM users AS u
+			WHERE u.id = $1
+		`,
+		id,
+	).Scan(columns...)
+
+	if err != nil {
+		return nil, &customerrors.UserNotFoundError{}
+	}
+
+	return user, nil
 }
 
 func (repo *CommonUsersRepository) GetUserByEmail(email string) (*entities.User, error) {
-	return nil, &customerrors.UserNotFoundError{}
+	user := &entities.User{}
+	columns := database.GetEntityColumns(user)
+	connection := repo.DBConnector.GetConnection()
+	err := connection.QueryRow(
+		`
+			SELECT * 
+			FROM users AS u
+			WHERE u.email = $1
+		`,
+		email,
+	).Scan(columns...)
+
+	if err != nil {
+		return nil, &customerrors.UserNotFoundError{}
+	}
+
+	return user, nil
 }
 
 func (repo *CommonUsersRepository) GetAllUsers() ([]*entities.User, error) {
-	return []*entities.User{}, nil
+	connection := repo.DBConnector.GetConnection()
+	rows, err := connection.Query(
+		`
+			SELECT * 
+			FROM users
+		`,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var users []*entities.User
+	for rows.Next() {
+		user := &entities.User{}
+		columns := database.GetEntityColumns(user)
+		err = rows.Scan(columns...)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	if err = rows.Close(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/DKhorkov/hmtm-sso/entities"
+	"github.com/DKhorkov/hmtm-sso/internal/config"
 	customerrors "github.com/DKhorkov/hmtm-sso/internal/errors"
 	"github.com/DKhorkov/hmtm-sso/internal/interfaces"
 	"github.com/DKhorkov/hmtm-sso/internal/security"
@@ -10,6 +11,7 @@ import (
 type CommonAuthService struct {
 	AuthRepository  interfaces.AuthRepository
 	UsersRepository interfaces.UsersRepository
+	JWTConfig       config.JWTConfig
 }
 
 func (service *CommonAuthService) LoginUser(userData entities.LoginUserDTO) (string, error) {
@@ -22,8 +24,12 @@ func (service *CommonAuthService) LoginUser(userData entities.LoginUserDTO) (str
 		return "", &customerrors.InvalidPasswordError{}
 	}
 
-	// TODO should be changed on JWT
-	return "someToken", nil
+	return security.GenerateJWT(
+		user,
+		service.JWTConfig.SecretKey,
+		service.JWTConfig.TTL,
+		service.JWTConfig.Algorithm,
+	)
 }
 
 func (service *CommonAuthService) RegisterUser(userData entities.RegisterUserDTO) (int, error) {
