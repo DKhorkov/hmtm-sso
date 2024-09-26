f25e195c78c85d6f702cfd489f220158fb220d98
DKhorkov <dkhorkov.dev@gmail.com>
2024-09-26 15:17:55 +0300

    main fix

--- internal/errors/auth_errors.go
+++ internal/errors/auth_errors.go
@@ -12,18 +12,6 @@ func (e *UserNotFoundError) Error() string {
 	return "user not found"
 }
 
-type UserAlreadyExistsError struct {
-	message string
-}
-
-func (e *UserAlreadyExistsError) Error() string {
-	if e.message != "" {
-		return e.message
-	}
-
-	return "user with this email already exists"
-}
-
 type InvalidPasswordError struct {
 	message string
 }
