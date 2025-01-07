c6cf7c6c670b927e35607aa79e469b26bbfed0d8
DKhorkov <dkhorkov.dev@gmail.com>
2025-01-08 00:19:08 +0300

    [HMTM-103] added telegramConfirmed field to User

---
+++ internal/controllers/grpc/users/output_preparators.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"github.com/DKhorkov/hmtm-sso/api/protobuf/generated/go/sso"
+	"github.com/DKhorkov/hmtm-sso/internal/entities"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func prepareUserOut(user *entities.User) *sso.GetUserOut {
+	return &sso.GetUserOut{
+		ID:                user.ID,
+		DisplayName:       user.DisplayName,
+		Email:             user.Email,
+		EmailConfirmed:    user.EmailConfirmed,
+		Phone:             user.Phone,
+		PhoneConfirmed:    user.PhoneConfirmed,
+		Telegram:          user.Telegram,
+		TelegramConfirmed: user.TelegramConfirmed,
+		Avatar:            user.Avatar,
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+		UpdatedAt:         timestamppb.New(user.UpdatedAt),
+	}
+}
