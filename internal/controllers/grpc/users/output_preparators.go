c86b7ee6abe35b05ebaa8fe62aebc7db922d2c70
DKhorkov <dkhorkov.dev@gmail.com>
2025-01-14 20:59:57 +0300

    [HMTM-110] - updated processing functions in idiomatic for go style

--- internal/controllers/grpc/users/output_preparators.go
+++ internal/controllers/grpc/users/output_preparators.go
@@ -6,7 +6,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func prepareUserOut(user *entities.User) *sso.GetUserOut {
+func prepareUserOut(user entities.User) *sso.GetUserOut {
 	return &sso.GetUserOut{
 		ID:                user.ID,
 		DisplayName:       user.DisplayName,
